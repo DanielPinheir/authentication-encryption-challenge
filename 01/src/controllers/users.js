2545eb5d723649c98f6d3c2a0125d2a2d32c1037
Daniel Pinheiro Machado <dyelzim@gmail.com>
2023-07-23 18:43:47 -0300

    create token

--- 01/src/controllers/users.js
+++ 01/src/controllers/users.js
@@ -1,6 +1,7 @@
 const pool = require("../connection");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const passwordJwt = require("../passwordJwt");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -23,6 +24,41 @@ const registerUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await pool.query(
+      `
+        select * from users
+        where email = $1
+      `,
+      [email]
+    );
+
+    if (user.rowCount < 1) {
+      return res.status(404).json({ message: "Email or password invalid." });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+    if (!validPassword) {
+      return res.status(400).json({ message: "Email or password invalid." });
+    }
+
+    const token = jwt.sign({ id: user.rows[0].id }, passwordJwt, {
+      expiresIn: "8h",
+    });
+
+    const { password: _, ...loggedUser } = user.rows[0];
+
+    return res.json({ user: loggedUser, token });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   registerUser,
+  login,
 };
--- 01/src/routes/routes.js
+++ 01/src/routes/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 
-const { registerUser } = require("../controllers/users");
+const { registerUser, login } = require("../controllers/users");
 
 const route = Router();
 
 route.post("/user", registerUser);
+route.post("/login", login);
 
 module.exports = route;
